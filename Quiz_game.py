d92e151fb8d4f1dfaf472604f02806a9f8d23ff4
UJJWAL NANDI <166310578+codingwithujjwal@users.noreply.github.com>
2024-08-11 01:48:48 +0530

    Update Quiz_game.py
    This is a fun game. The game asks some random questions, and if you give the correct answer, your score increases; otherwise, your score decreases.

--- Quiz_game.py
+++ Quiz_game.py
@@ -1,40 +1,40 @@
-print("welcome to my computer quiz!")
+print("Welcome to my computer quiz!")
 
-playing=input("Do you want to play?\n")
+playing = input("Do you want to play?\n")
 
-if playing.lower() !="yes":
+if playing.lower() != "yes":
     quit()
 
-print("Okey! Let's play:")
-#score=0
+print("Okay! Let's play:")
+score = 0  # Initialize the score variable
 
-answer=input("what does cpu stand for?\n")
-if answer.lower()=="central processing unit":
-    print("correct")
-    score+=1
+answer = input("What does CPU stand for?\n")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
 else:
-    print("Incorrect")
-    
-answer=input("what does gpu stand for?\n")
-if answer.lower()=="graphics processing unit":
-    print("correct")
-    score+=1
+    print("Incorrect!")
+
+answer = input("What does GPU stand for?\n")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
 else:
-    print("Incorrect")  
-    
-answer=input("what does RAM stand for?\n")
-if answer.lower()=="random access memory":
-    print("correct")
-    score+=1
+    print("Incorrect!")
+
+answer = input("What does RAM stand for?\n")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
 else:
-    print("Incorrect")    
-    
-answer=input("what does PSU stand for?\n")
-if answer.lower()=="power supply":
-    print("correct")
-    score+=1 
+    print("Incorrect!")
+
+answer = input("What does PSU stand for?\n")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score += 1
 else:
-    print("Incorrect")    
-    
+    print("Incorrect!")
+
 print("You got " + str(score) + " questions correct!")
 print("You got " + str((score / 4) * 100) + "%.")
